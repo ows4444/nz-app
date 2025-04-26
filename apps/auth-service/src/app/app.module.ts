1810188a195e76b0a0679c6fb32906a5b3898ad8
Muhammad Awais <ows4444@gmail.com>
2025-04-26 19:13:32 +0500

    feat: integrate MySQL database with Docker setup; enhance application configuration and Swagger documentation

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { Module, Provider } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthService } from '@nz/application-auth';
-import { SharedConfigModule } from '@nz/config';
+import { SharedConfigModule, TypeOrmEnvironment } from '@nz/config';
 import { USER_ACCOUNT_REPOSITORY } from '@nz/domain-auth';
 import { UserAccountEntityORM } from '@nz/infrastructure-auth';
 import { AuthController } from '@nz/presentation-auth';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.getOrThrow<TypeOrmEnvironment>('typeorm'),
+        entities: [UserAccountEntityORM],
+      }),
+      imports: [ConfigModule],
+    }),
     SharedConfigModule.forRoot({
       isGlobal: true,
       expandVariables: true,

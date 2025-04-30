e583edd24cf0197be7eb633d9e2ef56f0727d2e7
Muhammad Awais <ows4444@gmail.com>
2025-05-01 00:08:45 +0500

    feat: implement initial setup for api-gateway and user-service with configurations, modules, and tests

--- apps/user-service/src/app/app.module.ts
+++ apps/user-service/src/app/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { SharedConfigModule, TypeOrmEnvironment } from '@nz/config';
+import { UserAccountEntityORM } from '@nz/infrastructure-auth';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.getOrThrow<TypeOrmEnvironment>('typeorm'),
+        entities: [UserAccountEntityORM],
+      }),
+      imports: [ConfigModule],
+    }),
+    SharedConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+    }),
+  ],
+  // controllers: [AuthController],
+  // providers: ([] as Provider[]).concat(
+  //   [
+  //     AuthService,
+  //     {
+  //       provide: USER_ACCOUNT_REPOSITORY,
+  //       useClass: TypeormUserAccountRepository,
+  //     },
+  //   ],
+  //   UseCases,
+  // ),
 })
 export class AppModule {}
--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,14 +1,47 @@
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { Environment } from '@nz/config';
+import { LOGGER_SERVICE, LoggerService } from '@nz/logger';
 import { AppModule } from './app/app.module';
+import { BootstrapSwagger } from './bootstrap/swagger.bootstrap';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+async function Bootstrap() {
+  const app = await NestFactory.create<NestApplication>(AppModule, {
+    bufferLogs: true,
+  });
+  const logger = new Logger(Bootstrap.name);
+  const loggerService: LoggerService = app.get(LOGGER_SERVICE);
+
+  app.useLogger(loggerService);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.flushLogs();
+  const configService = app.get(ConfigService);
+  const { port, host, corsOrigins = ['*'] } = configService.getOrThrow<Environment>('env');
+
+  const globalPrefix = configService.get<string>('API_PREFIX') || 'api';
+
+  app.enableCors({
+    origin: corsOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+  });
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+
+  const isProduction = configService.getOrThrow<string>('NODE_ENV') === 'production';
+
+  if (isProduction) {
+    app.enableShutdownHooks();
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  } else {
+    BootstrapSwagger(app);
+  }
+
+  await app.listen(port, host);
+  logger.log(`🚀 Application is running on: http://${host}:${port}/${globalPrefix}`);
 }
 
-bootstrap();
+Bootstrap();

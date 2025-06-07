9aa025514a257cb9446b8fc56c5c21aa2ebc5a9f
Muhammad Awais <ows4444@gmail.com>
2025-06-07 12:22:56 +0500

    feat: enhance authentication flow by adding user registration and updating service client references

--- apps/api-gateway/src/app/auth.controller.ts
+++ apps/api-gateway/src/app/auth.controller.ts
@@ -1,25 +1,32 @@
+import { Metadata } from '@grpc/grpc-js';
 import { Body, Controller, Inject, OnModuleInit, Post } from '@nestjs/common';
 import type { ClientGrpc } from '@nestjs/microservices';
-import { LoginByEmailDto, LoginByUsernameDto } from '@nz/auth-session-presentation';
+import { LoginByEmailDto, LoginByUsernameDto, RegisterDto } from '@nz/auth-session-presentation';
+import { GrpcLangMetadata } from '@nz/shared-infrastructure';
 import { authSession } from '@nz/shared-proto';
 
 @Controller('auth')
 export class AuthController implements OnModuleInit {
-  private iamServiceClient!: authSession.AuthServiceClient;
+  private authServiceClient!: authSession.AuthServiceClient;
 
   constructor(@Inject(authSession.protobufPackage) private readonly grpcClient: ClientGrpc) {}
 
   onModuleInit() {
-    this.iamServiceClient = this.grpcClient.getService<authSession.AuthServiceClient>(authSession.AUTH_SERVICE_NAME);
+    this.authServiceClient = this.grpcClient.getService<authSession.AuthServiceClient>(authSession.AUTH_SERVICE_NAME);
   }
 
   @Post('login/email')
   authenticate(@Body() loginRequest: LoginByEmailDto) {
-    return this.iamServiceClient.loginByEmail(loginRequest);
+    return this.authServiceClient.loginByEmail(loginRequest);
   }
 
   @Post('login/username')
   authenticateByUsername(@Body() loginRequest: LoginByUsernameDto) {
-    return this.iamServiceClient.loginByUsername(loginRequest);
+    return this.authServiceClient.loginByUsername(loginRequest);
+  }
+
+  @Post('register')
+  register(@Body() loginRequest: RegisterDto, @GrpcLangMetadata() acceptLangMeta: Metadata) {
+    return this.authServiceClient.registerUser(loginRequest, acceptLangMeta);
   }
 }
--- apps/api-gateway/src/app/user.controller.ts
+++ apps/api-gateway/src/app/user.controller.ts
@@ -1,9 +1,6 @@
-import { Metadata } from '@grpc/grpc-js';
-import { Body, Controller, Inject, OnModuleInit, Post } from '@nestjs/common';
+import { Controller, Inject, OnModuleInit } from '@nestjs/common';
 import type { ClientGrpc } from '@nestjs/microservices';
-import { GrpcLangMetadata } from '@nz/shared-infrastructure';
 import { userDevice } from '@nz/shared-proto';
-import { RegisterDto } from '@nz/user-device-presentation';
 
 @Controller('user')
 export class UserController implements OnModuleInit {
@@ -13,10 +10,6 @@ export class UserController implements OnModuleInit {
 
   onModuleInit() {
     this.userServiceClient = this.grpcClient.getService<userDevice.UserServiceClient>(userDevice.USER_SERVICE_NAME);
-  }
-
-  @Post('register')
-  register(@Body() loginRequest: RegisterDto, @GrpcLangMetadata() acceptLangMeta: Metadata) {
-    return this.userServiceClient.register(loginRequest, acceptLangMeta);
+    console.log(this.userServiceClient == null ? 'User' : 'User Service Client Initialized');
   }
 }
